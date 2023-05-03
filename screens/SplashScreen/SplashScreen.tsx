e7988ade6b689a5f5e47fb2e9fb3d140c0b39d95
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-05-04 00:18:27 +0600

    project created

---
+++ screens/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Stack, Text } from 'tamagui'
+
+const SplashScreen = () => {
+  return (
+    <Stack>
+        <Text>Splash</Text>
+    </Stack>
+  )
+}
+
+export default SplashScreen
